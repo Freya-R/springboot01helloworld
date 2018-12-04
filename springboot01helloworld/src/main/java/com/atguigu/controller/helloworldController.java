1ad5a250c4c942be71bf8fd11dd4509d1ab8293d
Freya-R <2761611869@qq.com>
2018-12-04 12:40:57 +0800

    测试提交是否到正确仓库

--- springboot01helloworld/src/main/java/com/atguigu/controller/helloworldController.java
+++ springboot01helloworld/src/main/java/com/atguigu/controller/helloworldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloworldController {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println();
        return "hello world!";
     }
 }
